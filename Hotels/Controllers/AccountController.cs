e7509e8958f4181c53d857c3e311851f32c0eeb7
KnownNight <kasich.nszar@gmail.com>
2024-12-29 13:26:12 +0200

    hide admin pages

--- Hotels/Controllers/AccountController.cs
+++ Hotels/Controllers/AccountController.cs
@@ -45,7 +45,7 @@ namespace Hotels.Controllers
                 if (result.Succeeded)
                 {
                     // Добавляем пользователя в роль "User"
-                    var roleResult = await userManager.AddToRoleAsync(user, "Admin");
+                    var roleResult = await userManager.AddToRoleAsync(user, "User");
 
                     if (!roleResult.Succeeded)
                     {
